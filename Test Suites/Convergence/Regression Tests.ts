2d36b534c01c355cc2fa4c7bf890be230760a409
njones <njones@NJONES-LT0646.trinisys.loc>
2022-02-02 14:24:36 -0600

    Passing Regression Suite

--- Test Suites/Convergence/Regression Tests.ts
+++ Test Suites/Convergence/Regression Tests.ts
@@ -14,7 +14,7 @@
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Convergence/Setup</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
